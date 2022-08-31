4f4841414093befac40a35c9494512c43336eeab
Lucccii <paulin.manteau@gmail.com>
2022-09-01 01:13:29 +0200

    Ajout de la mise en connexion avec la base de données

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const moongose = require('mongoose')
 const app = express();
+app.use(express.json());
 
-app.use((req, res, next) => {
+
+moongose.connect('mongodb+srv://Lucccii:<Mm0634484228>@cluster0.ogom1im.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+console.log("Tout est ok")
+app.use('/api/stuff', (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use('/api/stuff', (req, res) => {
+
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message : 'Objet crée !'
+  });
+})
+
+app.get('/api/stuff', (req, res) => {
     const stuff = [
         {
           _id: 'oeihfzeoi',
