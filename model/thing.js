7699b0a969cf76702f110c838b9df041d4c6a759
Lucccii <paulin.manteau@gmail.com>
2022-09-05 15:27:35 +0200

    Ajout de la route get en vraie liaison avec base MongoDB

--- model/thing.js
+++ model/thing.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 
 const thingSchema = mongoose.Schema({
-    title : {type : String, required : true},
-    description : {type : String, required : true},
-    imageUrl : {type : String, required : true},
-    userId : {type : String, required : true},
-    number : {type : Number, required : true},
-});
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    userId: { type: String, required: true },
+    price: { type: Number, required: true },
+  });
+
+module.exports = mongoose.model("Thing", thingSchema);
 
 
-module.exports = mongoose.model("thing", thingSchema);
+app.use("/api/suff", (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({error}))
+})
